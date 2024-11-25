66b94234a6bb71b973bb5bad361f06301eaa63ad
Juan Grajales A. <131324102+jgrajalesa@users.noreply.github.com>
2024-11-25 15:04:57 -0500

    Add files via upload

---
+++ mi_primera_app.py
@@ -0,0 +1,14 @@
+import streamlit as st
+
+# Título de la app
+st.title('Mi primera app')
+
+# Autor de la app
+st.write('Esta app fue elaborada por COLOQUE AQUÍ SU NOMBRE')
+
+# Pregunta al usuario
+nombre_usuario = st.text_input('¿Cuál es tu nombre?')
+
+# Mostrar el mensaje de bienvenida
+if nombre_usuario:
+    st.write(f'{nombre_usuario}, te doy la bienvenida a mi primera app.')
